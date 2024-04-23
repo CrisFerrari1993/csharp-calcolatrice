462763ff7f08df4a9aa20095a4d667e31f8358f4
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-23 15:08:53 +0200

    Consegna

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    static class CalcoliHelper
+    {
+        
+        // i metodi ripetuti piu volte negli oggetti vengono definiti overload di metodi 🤓
+        // I parametri sono stati costruiti per accettare float, double, int, decimal, long.
+        public static string Somma<T>(T n1, T n2) where T : INumber<T>
+        {
+            return $"La somma di {n1} e {n2} è di: {n1 + n2}";
+        }
+        public static string Differenza<T>(T n1, T n2) where T : INumber<T>
+        {
+            return $"La differenza fra {n1} e {n2} è di: {n1 - n2}";
+        }
+        public static string Moltiplicazione<T>(T n1, T n2) where T : INumber<T>
+        {
+            return $"Il risultato della moltiplicazione fra {n1} e {n2} è di: {n1 * n2}";
+        }
+        public static string ValoreABS(int n)
+        {
+            return $"Il valore assoluto di {n} è di: {Math.Abs(n)}";
+        }
+        public static string ValoreABS(double n)
+        {
+            return $"Il valore assoluto di {n} è di: {Math.Abs(n)}";
+        }
+        public static string Minimo<T>(T n1, T n2) where T : INumber<T>
+        {
+            if (n1 < n2) return $"Il valore minimo fra {n1} e {n2} è: {n1}";
+            else return $"Il valore minimo fra {n1} e {n2} è: {n2}";
+        }
+        public static string Massimo<T>(T n1, T n2) where T : INumber<T>
+        {
+            if (n1 > n2) return $"Il valore massimo fra {n1} e {n2} è: {n1}";
+            else return $"Il valore massimo fra {n1} e {n2} è: {n2}";
+        }
+        // **************BONUS*****************
+        public static string Potenza(int Base, int Esponente)
+        {
+            double res = 1;
+
+            //Se l'esponente è positivo, moltiplica la base per se stessa esponente volte
+            if(Esponente > 0)
+            {
+                for(int i = 0; i < Esponente; i++)
+                {
+                    res *= Base;
+                }
+            }
+            // Se l'esponente è negativo, calcola l'inverso della potenza positiva
+            else if (Esponente < 0)
+            {
+                for (int i = 0; i < Math.Abs(Esponente); i++)
+                {
+                    res /= Base;
+                }
+            } 
+            // Se l'esponente è zero, il risultato è 1
+            // (poiché qualsiasi numero elevato alla potenza 0 è uguale a 1)
+
+            return $"La potenza di {Base} alla {Esponente} è di: {(int)res}";
+        }
+    }
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -1,10 +1,91 @@
-﻿namespace csharp_calcolatrice
+﻿using System.Reflection.Metadata;
+
+namespace csharp_calcolatrice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string userChoice = "";
+            
+            while (userChoice != "esci")
+            {
+                Console.WriteLine("Scegli il tipo di operazione fra (1/Somma, 2/Differenza, 3/Moltiplicazione, 4/Valore assoluto, 5/Valore minimo, 6/Valore massimo, 7/Potenza)");
+                userChoice = Console.ReadLine();
+                switch (userChoice)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Somma i numeri");
+                            Console.WriteLine("Inserisci un numero");
+                            var n1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Inserisci un'altro numero");
+                            var n2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine(CalcoliHelper.Somma(n1, n2));
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("Differenza fra i numeri");
+                            Console.WriteLine("Inserisci un numero");
+                            var n1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Inserisci un'altro numero");
+                            var n2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine(CalcoliHelper.Differenza(n1, n2));
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Moltiplica i numeri");
+                            Console.WriteLine("Inserisci un numero");
+                            var n1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Inserisci un'altro numero");
+                            var n2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine(CalcoliHelper.Moltiplicazione(n1, n2));
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.WriteLine("Valore assoluto di un numero");
+                            Console.WriteLine("Inserisci un numero");
+                            var n1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine(CalcoliHelper.ValoreABS(n1));
+                            break;
+                        }
+                    case "5":
+                        {
+                            Console.WriteLine("Valore minimo fra due numeri");
+                            Console.WriteLine("Inserisci un numero");
+                            var n1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Inserisci un'altro numero");
+                            var n2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine(CalcoliHelper.Minimo(n1, n2));
+                            break;
+                        }
+                    case "6":
+                        {
+                            Console.WriteLine("Valore massimo fra due numeri");
+                            Console.WriteLine("Inserisci un numero");
+                            var n1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Inserisci un'altro numero");
+                            var n2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine(CalcoliHelper.Massimo(n1, n2));
+                            break;
+                        }
+                    case "7":
+                        {
+                            Console.WriteLine("Potenza di Un numero per esponente");
+                            Console.WriteLine("Inserisci un numero per la base");
+                            var n1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Inserisci un'altro numero per l'esponente");
+                            var n2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine(CalcoliHelper.Potenza(n1, n2));
+                            break;
+                        }
+                }
+            }
+
         }
     }
 }
